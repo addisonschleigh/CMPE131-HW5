5ec2acf7a25c5c154319d000b5153c8c6243af11
addisonschleigh <addisonschleigh@icloud.com>
2025-11-11 17:00:26 -0800

    Added first unit tests

--- tests/unit/unit_test.py
+++ tests/unit/unit_test.py
@@ -1,6 +1,11 @@
 import pytest
-from ...src.pricing import parse_price
+
+from src.pricing import parse_price
 
 def test_valid_parse_price():
     parsed_price = parse_price("$1,234.5")
     assert parsed_price == 1234.5
+
+def test_invalid_parse_price():
+    with pytest.raises(ValueError):
+        parse_price("abc")
